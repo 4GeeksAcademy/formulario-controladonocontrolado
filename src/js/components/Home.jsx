e1e284184b6241a965cada22c8d606d4d23ac2b3
Manuel Pernia <manuelk211995@gmail.com>
2026-03-16 11:45:44 +0000

    formlarioTareas

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,88 @@
-import React from "react";
+
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+
+	const [tareas, setTareas] = useState([]);
+	function borrarTareas(index) {
+
+		setTareas(tareas.filter((_, i) => i !== index))
+
+
+
+	}
+
+
+	const agregarTareas = ev => {
+
+
+		ev.preventDefault();
+
+		const nuevaTarea = ev.target.tarea.value;
+
+		if (!nuevaTarea.trim()) return;
+
+		setTareas([...tareas, nuevaTarea]);
+
+		ev.target.reset();
+
+
+
+	}
+
+
+
+
+	return (
+		<form
+
+			onSubmit={agregarTareas}
+			className="formulario"
+
+		>
+
+			<h2>Mis Tareas</h2>
+
+			<div className="campo">
+
+
+				<input type="text" name="tarea" placeholder="Escribe una tarea" />
+
+			</div>
+
+
+			<ul className="lista">
+				{tareas.map((item, index) => (
+					<li key={index}>
+
+						<p>{item}<span onClick={() => borrarTareas(index)}>X</span></p>
+
+					</li>
+				))}
+			</ul>
+
+			<p className="contador">Tareas: {tareas.length}</p>
+
+
+
+
+
+		</form>
+
+
+
+
+
+
+
+
+
+
 	);
 };
 
